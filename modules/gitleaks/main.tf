32bc9fb5aedce383732d01f870ede102cd5fd8f6
Petko D. Petkov <pdp@gnucitizen.org>
2018-11-09 11:24:04 +0000

    Make it verbose.

--- modules/gitleaks/main.tf
+++ modules/gitleaks/main.tf
@@ -4,7 +4,7 @@ module "main" {
   schedule = "${var.schedule}"
 
   task_name = "opendevsecops_scanner_gitleaks"
-  task_args = ["-r", "${var.target}"]
+  task_args = ["-v", "-r", "${var.target}"]
 
   trigger_arn = "${var.trigger_arn}"
 }
